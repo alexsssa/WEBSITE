608ffed3b80efa02fc091d0fbce698d5cec2c530
alicia <Safonova14092004@yandex.ru>
2022-10-17 19:11:30 +0400

    Файл main

--- main.py
+++ main.py
@@ -25,7 +25,5 @@ def secret_page():
 def contents_page():
     return render_template('contents-page.html' )
 
-#if __name__ == '__main__':
-#    app.run(port=8080, host='127.0.0.1')
-#    port = int(os.environ.get("PORT", 5000))
-#    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+ app.run()
