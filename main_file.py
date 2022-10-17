80ca2377f6ffb7e84391b4d61c33b74734691ef2
alicia <Safonova14092004@yandex.ru>
2022-10-17 17:26:35 +0400

    Файл сервера

---
+++ main_file.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('main-page.html')
+
+@app.route('/info-page')
+def info_page():
+    return render_template('info-page.html')
+
+@app.route('/secret-page', methods=['post', 'get'])
+def secret_page():
+    if request.method == 'POST':
+        password = request.form.get('password')
+        if password == 'qwerty':
+            return render_template('contents-page.html')
+        else:
+            return render_template('main-page.html')
+    return render_template('secret-page.html')
+
+@app.route('/contents-page')
+def contents_page():
+    content_text = [
+        "https://getmdl.io/components/  -   Документация по Material Design by Google",
+        "http://flask.pocoo.org/docs/1.0/   -   Документация по Flask"
+    ]
+    return render_template('contents-page.html' )
+
+if __name__ == '__main__':
+    app.run(port=8081, host='127.0.0.1')
+ #port = int(os.environ.get("PORT", 5000))
+ #app.run(host='0.0.0.0', port=port)
