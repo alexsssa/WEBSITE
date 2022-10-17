2de6162c2502ae615a9a8df8ee32591614884665
alicia <Safonova14092004@yandex.ru>
2022-10-17 19:51:45 +0400

    Подгрузка порта

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -26,4 +27,6 @@ def contents_page():
     return render_template('contents-page.html' )
 
 if __name__ == '__main__':
- app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
