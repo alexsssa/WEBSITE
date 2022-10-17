cca6842f4fc862afc2248e6a6765a8473e7f6624
alicia <Safonova14092004@yandex.ru>
2022-10-17 17:33:48 +0400

    Файл сервера

--- main_file.py
+++ main_file.py
@@ -30,4 +30,6 @@ def contents_page():
     return render_template('contents-page.html' )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+    #app.run(port=8081, host='127.0.0.1')
+ port = int(os.environ.get("PORT", 5000))
+ app.run(host='0.0.0.0', port=port)
